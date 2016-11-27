849cb7d7c7a7bb7330b6d5a5c4fdc89545e1d9ca
Vu Diep <Vu Diep>
2016-11-27 15:00:36 +0700

    CollectionUtils

--- src/main/java/onboarding/collection/CollectionUtils.java
+++ src/main/java/onboarding/collection/CollectionUtils.java
@@ -5,11 +5,8 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.IntStream;
 
 public class CollectionUtils {
 	/**
